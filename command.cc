cb59c4011a7543eda25ec62178ab14baf188ad13
Alex Brooke <akb.sbc@gmail.com>
2018-09-25 15:15:23 -0400

    Commit

--- command.cc
+++ command.cc
@@ -111,11 +111,11 @@ void Command::execute() {
 
     // int ifd = 0;
     int ofd = 1;
-    // int efd = 2;
+    int efd = 2;
 
     // int stdinfd = dup(ifd);
     int stdoutfd = dup(ofd);
-    // int stderrfd = dup(efd);
+    int stderrfd = dup(efd);
 
     for (auto cmd : _simpleCommands) {
       pid = fork();
@@ -126,8 +126,8 @@ void Command::execute() {
       }
 
       // only need to open output fd once if out and err go to same place
-      // if ((_outFile || _errFile) && _outFile == _errFile) ofd = efd = creat(_outFile->c_str(), 0666);
-      /*else*/ if (_outFile) ofd = creat(_outFile->c_str(), 0666);
+      if ((_outFile || _errFile) && _outFile == _errFile) ofd = efd = creat(_outFile->c_str(), 0666);
+      else if (_outFile) ofd = creat(_outFile->c_str(), 0666);
       // else if (_errFile) efd = creat(_errFile->c_str(), 0666);
 
       // if (_inFile) ifd = creat(_inFile->c_str(), 0666);
@@ -137,15 +137,21 @@ void Command::execute() {
       //   exit(2);
       // }
       if (ofd < 0) {
-        perror("fatal: creat output file");
+        perror("fatal: creat output file\n");
         exit(2);
       }
       if (int e1 = dup2(ofd, 1); e1 == -1) {
         perror("fatal: redirect stdout\n");
+        exit(2);
       }
-      // if (int e2 = dup2(stderrfd, efd); e2 == -1) {
-      //   perror("fatal: redirect stderr\n");
-      // }
+      if (efd < 0) {
+        perror("fatal: creat error file\n");
+        exit(2);
+      }
+      if (int e2 = dup2(efd, 1); e2 == -1) {
+        perror("fatal: redirect stderr\n");
+        exit(2);
+      }
 
 
       // TODO: pipe
@@ -154,7 +160,7 @@ void Command::execute() {
       if (pid == 0) {
         // close(stdinfd);
         close(stdoutfd);
-        // close(stderrfd);
+        close(stderrfd);
 
         // special thanks to https://stackoverflow.com/questions/48727690/invalid-conversion-from-const-char-to-char-const
         std::vector<char*> argv;
@@ -163,10 +169,10 @@ void Command::execute() {
 
         execvp(cmd->_arguments[0]->c_str(), argv.data());
       }
-      dup2(stdoutfd, 1);
-      // close(stdoutfd);
     }
     // restore stdin, stdout, stderr
+    dup2(stdoutfd, 1);
+    dup2(stderrfd, 2);
     // dup2(0, stdinfd);
     // dup2(2, stderrfd);
 
